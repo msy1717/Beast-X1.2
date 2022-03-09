ef9b28ff29594663ec287922084c6a99ed3c0a22
Arjun <86394506+Arjun-M@users.noreply.github.com>
2022-03-09 15:53:15 +0530

    Update __init__.py

--- beastx/function/__init__.py
+++ beastx/function/__init__.py
@@ -3,3 +3,11 @@ from PIL import Image, ImageDraw, ImageFont
 
 ZONE = pytz.timezone( os.environ.get("TIMEZONE", "Asia/Kolkata") )
 TIME = datetime.datetime.now(ZONE).strftime('%I:%M %p')
+
+async def drawProfile():
+    img = Image.open("resources/image.jpg")
+    font = ImageFont.truetype("resources/ds-digit.ttf", 360)
+    to_edit = ImageDraw.Draw( img )
+    to_edit.text((690, 550), TIME , (0, 255, 255), font = font )
+    img.save("resources/logo.jpg")
+    return True
