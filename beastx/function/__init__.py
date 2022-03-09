cca8a41327739bf2354c1b63e097e3af258bd03b
Arjun <86394506+Arjun-M@users.noreply.github.com>
2022-03-09 15:46:31 +0530

    Update __init__.py

--- beastx/function/__init__.py
+++ beastx/function/__init__.py
@@ -1,4 +1,5 @@
-import asyncio , json , datetime , random , os
+import asyncio , json , datetime , random , os , pytz , time
 from PIL import Image, ImageDraw, ImageFont
 
-  
+ZONE = pytz.timezone( os.environ.get("TIMEZONE", "Asia/Kolkata") )
+TIME = datetime.datetime.now(ZONE).strftime('%I:%M %p')
